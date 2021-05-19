1daf16f5c1d19fa1f43067d507be8c406308ea0e
vinaygshet <80429916+vinaygshet@users.noreply.github.com>
2021-05-19 15:57:39 +0530

    Add files via upload
    - Get all the countries from Asia continent /region using Filter function

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// get all the countries with population of less than 2lakh using filter function
+//- Get all the countries from Asia continent /region using Filter function
 var request = new XMLHttpRequest();
 request.open('GET','https://restcountries.eu/rest/v2/all',true);
 request.send();
 request.onload=function(){
     var countrydata = JSON.parse(this.response);
-    var country = countrydata.filter((ele)=>{
-        return ele.population<=200000;
-    
-    });
-    console.log(country);
-};
+    var countriesFromAsia = countrydata.filter((x)=>x.region =='Asia');
+    console.log(countriesFromAsia);
+}
