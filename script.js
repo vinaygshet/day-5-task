f190b7e2f14802e6e4aec50e63ccfab61e01baea
vinaygshet <80429916+vinaygshet@users.noreply.github.com>
2021-05-19 16:23:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+//- Get all the countries from Asia continent /region using Filter function
+var request = new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var countrydata = JSON.parse(this.response);
+    var countriesFromAsia = countrydata.filter((x)=>x.region =='Asia');
+    console.log(countriesFromAsia);
+}
+// get all the countries with population of less than 2lakh using filter function
+var request = new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var countrydata = JSON.parse(this.response);
+    var country = countrydata.filter((ele)=>{
+        return ele.population<=200000;
+    
+    });
+    console.log(country);
+};
+// - Print the following details name, capital, flag using forEach function 
+var request = new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var data = JSON.parse(this.response);
+    var result = data.forEach((item)=>{
+        console.log(item.name,item.capital,item.flag);
+    });
+    
+};
+// -  Print the total population of countries using reduce function 
+var request = new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var countrydata = JSON.parse(this.response);
+    var country = countrydata.reduce((acc,country)=>{
+        return acc+country.population;
+    },0);
+    console.log(country);
+};
 //- Print the country which use US Dollars as currency
 var request = new XMLHttpRequest();
 request.open('GET','https://restcountries.eu/rest/v2/all',true);
