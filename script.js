ee765d4e678e88d1c0c9857395646a93285f07df
vinaygshet <80429916+vinaygshet@users.noreply.github.com>
2021-05-19 15:59:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-//- Get all the countries from Asia continent /region using Filter function
-var request = new XMLHttpRequest();
-request.open('GET','https://restcountries.eu/rest/v2/all',true);
-request.send();
-request.onload=function(){
-    var countrydata = JSON.parse(this.response);
-    var countriesFromAsia = countrydata.filter((x)=>x.region =='Asia');
-    console.log(countriesFromAsia);
-}
